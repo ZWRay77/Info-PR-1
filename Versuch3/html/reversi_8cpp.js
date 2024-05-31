4963ec92250b108cbd210f4598c20f881d290890
ZWRay77 <121447292+ZWRay77@users.noreply.github.com>
2024-05-31 18:12:11 +0200

    Add files via upload

---
+++ Versuch3/html/reversi_8cpp.js
@@ -0,0 +1,16 @@
+var reversi_8cpp =
+[
+    [ "aufSpielfeld", "reversi_8cpp.html#a6ded08a0017d5e8a5909d8ce403058fa", null ],
+    [ "gewinner", "reversi_8cpp.html#ab5b9afa274ecde39b15429f5fc77b59e", null ],
+    [ "initialisiereSpielfeld", "reversi_8cpp.html#a5c2aa50af4bb13bc589b835e1c1473bc", null ],
+    [ "main", "reversi_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4", null ],
+    [ "menschlicherZug", "reversi_8cpp.html#ae44cf481fe71ebc30511c76190e6bc5d", null ],
+    [ "moeglicheZuege", "reversi_8cpp.html#a8cf3121558de67463e88957becd116d1", null ],
+    [ "neueSpielen", "reversi_8cpp.html#a2ad27ae48ab49bde3fea4278e8f9dc60", null ],
+    [ "selectSpielType", "reversi_8cpp.html#ad39e3c1c0f327eb5bdfb4ef4b9f915fd", null ],
+    [ "spielen", "reversi_8cpp.html#a596345e24a803c4d6cc0880cdd9c6788", null ],
+    [ "testmain", "reversi_8cpp.html#a1830a82d92633d574ca517ed92bc2b2d", null ],
+    [ "zeigeSpielfeld", "reversi_8cpp.html#ace1027b2745225e26a5ffb243cf24a15", null ],
+    [ "zugAusfuehren", "reversi_8cpp.html#a7034afa60f015a8ab6c0b836d316d4a4", null ],
+    [ "zugGueltig", "reversi_8cpp.html#a0f17ff75622987b979767417e76164a0", null ]
+];
